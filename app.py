20b5fd847d967396c660174b93e3ae16f7c10269
Dennis Wu <dennis_h_wu@yahoo.com>
2016-09-24 15:42:58 -0700

    a

--- app.py
+++ app.py
@@ -7,7 +7,27 @@ from routes import *
 
 wsgi_app = app
 
-if __name__ == "__main__":
+@app.route('/about/')
+def about():
+    """Render the website's about page."""
+    return render_template('about.html')
+
+
+###
+# The functions below should be applicable to all Flask apps.
+###
+
+@app.route('/<file_name>.txt')
+def send_text_file(file_name):
+    """Send your static text file."""
+    file_dot_text = file_name + '.txt'
+    return app.send_static_file(file_dot_text)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+if __name__ == "__main__NOT":
     import os
     host = os.environ.get('SERVER_HOST', '0.0.0.0')
     try:
